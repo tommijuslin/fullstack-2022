7ebdd1068e5c1cf5667a7f8209c8442ac19ceb19
tommijuslin <tommi.juslin@gmail.com>
2022-03-17 20:14:30 +0200

    kurssitiedot: erillinen moduuli

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,36 +1,4 @@
-const Header = ({ name }) => <h1>{name}</h1>
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-      <Total parts={parts} />
-    </div>
-  )
-}
-
-const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => {
-    return sum + part.exercises
-  }, 0)
-
-  return (
-    <b>Number of exercises {total}</b>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
