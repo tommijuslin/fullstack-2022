08e6f57c00240ad62adb03a75bbd3644cc82e081
tommijuslin <tommi.juslin@gmail.com>
2022-03-17 19:46:05 +0200

    kurssitiedot step6

---
+++ part2/kurssitiedot/src/App.js
@@ -0,0 +1,62 @@
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </div>
+  )
+}
+
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
