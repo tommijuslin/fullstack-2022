02ed8cde7038295043c98b87d6bbfa95ca204107
tommijuslin <tommi.juslin@gmail.com>
2022-03-24 19:18:30 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (acc, cur) => {
+    return acc + cur.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
