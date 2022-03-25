65a6935d6d5e8bc96eb1b16ad6c8d92b05ba17cb
tommijuslin <tommi.juslin@gmail.com>
2022-03-26 00:04:24 +0200

    4.17: blogilistan laajennus, step5

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -10,14 +12,17 @@ blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   if (!body.title || !body.url) {
-    return response.status(400).end()
+    return response.status(400).json({ error: 'title or url missing' })
   }
+
+  const users = await User.find({})
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes ? body.likes : 0
+    likes: body.likes ? body.likes : 0,
+    user: users[0]
   })
 
   const savedBlog = await blog.save()
