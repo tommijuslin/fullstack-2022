23fa87219b2aa07cc0dcdaeefc09c5b578f8f1d8
tommijuslin <tommi.juslin@gmail.com>
2022-03-26 19:09:15 +0200

    4.19: blogilistan laajennus, step7

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -8,21 +9,34 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
+  
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url missing' })
   }
 
-  const users = await User.find({})
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ? body.likes : 0,
-    user: users[0]
+    user: user._id
   })
 
   const savedBlog = await blog.save()
