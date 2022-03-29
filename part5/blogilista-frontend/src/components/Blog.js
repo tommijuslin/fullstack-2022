93daa5e745dffa6d468ff3239a89c727d137d63a
tommijuslin <tommi.juslin@gmail.com>
2022-03-29 09:17:17 +0300

    5.7* blogilistan frontend, step7

--- part5/blogilista-frontend/src/components/Blog.js
+++ part5/blogilista-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const [showFullInfo, setShowFullInfo] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showAllBlogInfo = () => {
+    return (
+      <div>
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+        </div>
+        <div>
+          {blog.likes} likes
+          <button>like</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setShowFullInfo(!showFullInfo)}>
+        {blog.title} {blog.author}
+        {
+          showFullInfo ? showAllBlogInfo() : []
+        }
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
