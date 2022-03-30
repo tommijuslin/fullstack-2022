739b88b9ece51859d92289176d9e5b1a4c47e6bd
tommijuslin <tommi.juslin@gmail.com>
2022-03-30 12:17:32 +0300

    5.18: blogilistan end to end -testit, step2

--- part5/blogilista-frontend/cypress/integration/blog_app.spec.js
+++ part5/blogilista-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,34 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user =  {
+      username: 'tommijuslin',
+      password: 'enkerro'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('tommijuslin')
+      cy.get('#password').type('enkerro')
+      cy.contains('login').click()
+
+      cy.contains('blogs')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('tommijuslin')
+      cy.get('#password').type('entieda')
+      cy.contains('login').click()
+
+      cy.contains('wrong username or password')
+        .should('have.css', 'color', 'rgb(254, 0, 0)')
+    })
   })
 })
--- part5/blogilista-frontend/src/components/LoginForm.js
+++ part5/blogilista-frontend/src/components/LoginForm.js
@@ -14,6 +14,7 @@ const LoginForm = ({
         <div>
           username
           <input
+            id='username'
             type="text"
             value={username}
             name="Username"
@@ -23,6 +24,7 @@ const LoginForm = ({
         <div>
           password
           <input
+            id='password'
             type="password"
             value={password}
             name="Password"
