38eee1f9eb1ba5528e9d0d72797c9bac69d5b4e6
tommijuslin <tommi.juslin@gmail.com>
2022-04-08 14:55:40 +0300

    8.7: Kirjailijan syntymävuoden päivitys

--- part8/library/index.js
+++ part8/library/index.js
@@ -119,6 +119,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -151,6 +152,18 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
