db2d4e264819242f75b245a333e56c98a7bf74d8
tommijuslin <tommi.juslin@gmail.com>
2022-04-08 14:00:43 +0300

    8.4: kirjailijan kirjat

--- part8/library/index.js
+++ part8/library/index.js
@@ -105,9 +105,9 @@ const typeDefs = gql`
   }
 
   type Query {
-    bookCount(name: String): Int!
+    bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -116,7 +116,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      args.author ? books.filter((book) => book.author === args.author) : books,
     allAuthors: () => authors,
   },
   Author: {
