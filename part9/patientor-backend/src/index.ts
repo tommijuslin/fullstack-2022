b2deb01d5c7bc3999c72af223d786e2de9b9ff81
tommijuslin <tommi.juslin@gmail.com>
2022-04-14 01:42:39 +0300

    9.10: Patientor backend, step3

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -10,6 +11,8 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/services/diagnosisService.ts
@@ -0,0 +1,13 @@
+import diagnosisData from '../data/diagnoses.json';
+
+import { Diagnosis } from '../types';
+
+const diagnoses: Array<Diagnosis> = diagnosisData;
+
+const getDiagnoses = (): Array<Diagnosis> => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses
+};
