7cc6ffe21af32032292bdfbf036f65762fe8b540
tommijuslin <tommi.juslin@gmail.com>
2022-04-13 07:12:50 +0300

    9.5 WebBMI

--- part9/part-b/bmiCalculator.ts
+++ part9/part-b/bmiCalculator.ts
@@ -18,7 +18,7 @@ const parseArgumentsBmi = (args: Array<string>): Measurements => {
   }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 18.5) {
     return 'Underweight';
--- part9/part-b/index.ts
+++ part9/part-b/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!height || !weight || !Number(height) || !Number(weight)) {
+    res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result = {
+    weight,
+    height,
+    bmi: calculateBmi(height, weight),
+  };
+
+  res.json(result);
 });
 
 const PORT = 3003;
