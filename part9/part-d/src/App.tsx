db584e57636990ad745ee35cdfaaca018d0ddffd
tommijuslin <tommi.juslin@gmail.com>
2022-04-14 12:42:23 +0300

    9.15

--- part9/part-d/src/App.tsx
+++ part9/part-d/src/App.tsx
@@ -2,48 +2,159 @@ interface HeaderProps {
   name: string;
 }
 
-interface ContentProps {
-  parts: { name: string; exerciseCount: number }[];
-}
-
 interface TotalProps {
   total: number;
 }
 
-const Header = (props: HeaderProps) => {
-  return <h1>{props.name}</h1>;
+interface ContentProps {
+  parts: CoursePart[];
+}
+
+interface PartProps {
+  courseParts: CoursePart;
+}
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CourseDescription {
+  type: 'normal';
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CourseDescription {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CourseDescription {
+  type: 'special';
+  requirements: string[];
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseSpecialPart;
+
+const Part = ({ courseParts }: PartProps) => {
+  switch (courseParts.type) {
+    case 'normal':
+      return (
+        <div>
+          <div style={{ fontWeight: 'bold' }}>
+            {courseParts.name} {courseParts.exerciseCount}
+          </div>
+          <div style={{ fontStyle: 'italic' }}>{courseParts.description}</div>
+        </div>
+      );
+    case 'groupProject':
+      return (
+        <div>
+          <div style={{ fontWeight: 'bold' }}>
+            {courseParts.name} {courseParts.exerciseCount}
+          </div>
+          project exercises {courseParts.groupProjectCount}
+        </div>
+      );
+    case 'submission':
+      return (
+        <div>
+          <div style={{ fontWeight: 'bold' }}>
+            {courseParts.name} {courseParts.exerciseCount}
+          </div>
+          <div style={{ fontStyle: 'italic' }}>{courseParts.description}</div>
+          submit to {courseParts.exerciseSubmissionLink}
+        </div>
+      );
+    case 'special':
+      return (
+        <div>
+          <div style={{ fontWeight: 'bold' }}>
+            {courseParts.name} {courseParts.exerciseCount}
+          </div>
+          <div style={{ fontStyle: 'italic' }}>{courseParts.description}</div>
+          required skills: {courseParts.requirements.join(', ')}
+        </div>
+      );
+    default:
+      return assertNever(courseParts);
+  }
 };
 
-const Content = (props: ContentProps) => {
+const Header = ({ name }: HeaderProps) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }: ContentProps) => {
   return (
     <div>
-      {props.parts.map((course) => (
-        <p key={course.name}>
-          {course.name} {course.exerciseCount}
-        </p>
+      {parts.map((part) => (
+        <div key={part.name}>
+          <Part courseParts={part} />
+          <br />
+        </div>
       ))}
     </div>
   );
 };
 
-const Total = (props: TotalProps) => {
-  return <p>Number of exercises {props.total}</p>;
+const Total = ({ total }: TotalProps) => {
+  return <p>Number of exercises {total}</p>;
+};
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
 };
 
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is the leisured course part',
+      type: 'normal'
+    },
+    {
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is the harded course part',
+      type: 'normal'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject'
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+      type: 'submission'
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      type: 'special'
     }
   ];
 
@@ -53,7 +164,7 @@ const App = () => {
   );
 
   return (
-    <div>
+    <div style={{ fontFamily: 'Arial' }}>
       <Header name={courseName} />
       <Content parts={courseParts} />
       <Total total={exerciseTotal} />
